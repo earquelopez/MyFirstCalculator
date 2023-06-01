61eac464b84bacb8818ac5bcee28f56dde3d2106
Eduardo Arqué López <earquelopez@gmail.com>
2023-06-01 14:09:48 +0200

    Added new configuration closes #1

---
+++ MyFirstCalculator/DotNetConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstCalculator
+{
+    internal class DotNetConfiguration
+    {
+        public string GetConfiguration()
+        {
+            return "new configuration";
+        }
+    }
+}
